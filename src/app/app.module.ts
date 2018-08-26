9bf8ce471cbefe95b685f5c2e5c06f2f2ad13def
Karabaza <karabaza.vlada@gmail.com>
2018-08-26 15:35:55 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TopComponent } from './top/top.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopComponent,
+    CarouselComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
