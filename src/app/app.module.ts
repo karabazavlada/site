acc8d3e21f85e446a556f8cab33758792974e8e4
Lera <karabaza.lera@yandex.ru>
2018-08-30 16:12:08 +0300

    routerlink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TopComponent } from './top/top.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
 
+
+const appRoutes: Routes =[
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -14,12 +21,18 @@ import { ContactsComponent } from './contacts/contacts.component';
     NavbarComponent,
     TopComponent,
     CarouselComponent,
-    ContactsComponent
+    ContactsComponent, 
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+
