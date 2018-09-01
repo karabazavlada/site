a4f9b53b3ca41981f8dbfb853ac0ced20e3fb854
Karabaza <karabaza.vlada@gmail.com>
2018-09-01 19:50:49 +0300

    page "parents"

--- src/app/parents/parents.component.ts
+++ src/app/parents/parents.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'parents-app',
   templateUrl: './parents.component.html',
-  styleUrls: ['./parents.component.css']
+  styleUrls: ['./parents.component.scss']
 })
 export class ParentsComponent implements OnInit {
 
