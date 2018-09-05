9e24e173ff5ac0af9065c4e4c7eb263025e0bfc6
Lera <karabaza.lera@yandex.ru>
2018-09-05 17:08:10 +0300

    final ver

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'friends-app',
   templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.css', 
+  styleUrls: ['./friends.component.scss', 
   '../app.component.scss']
 })
 export class FriendsComponent implements OnInit {
